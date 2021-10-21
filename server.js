cd67fe9fc682678c04a7c0256a89d1a70db14f2b
todjiaa <todjiaa@todjiaa.com>
2021-10-21 18:01:55 +0400

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // import express from "express";
-require("dotenv/config");
+// require("dotenv/config");
 const express = require('express');
 const session = require("express-session");
 const app = express();
